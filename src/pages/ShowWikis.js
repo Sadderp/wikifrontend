a8a579351bb25d0c32da7f2e5815ee8102de01a2
sadderp <cewin.eriksson@elev.ga.ntig.se>
2022-12-07 09:48:46 +0100

    startsida

--- src/pages/ShowWikis.js
+++ src/pages/ShowWikis.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { saveLS } from "../components/functions";
 import { loadLS } from "../components/functions";
@@ -12,29 +12,33 @@ function ShowWikis(props){
     if(state != null){
         error = state.error;
     }
+    let wikiList = [];
+    let result = {};
 
-    const sendData = () => {
-        const API_URL ="https://takeee.ntigskovde.se/Wiki/wiki_index.php?action=showWikis";
-        sendIt(API_URL);
-    }
+    const API_URL ="https://takeee.ntigskovde.se/Wiki/wiki_index.php?action=showWikis";
+    sendIt(API_URL);
+
     const sendIt = async (url) => {
         const response = await fetch(`${url}`);
         const data = await response.json();
+        let result = data;
         if(data.Type=="Ok"){
             navigate('/start', {state:{error:"Showing all wikis"}});
         } else if(data.Type=="Error"){
             navigate('/start', {state:{error:"Couldn't load wikis"}});
         }
-        data.array.forEach(data => {
-            console.log(data.Data['title']);    
-        });
+        showWiki();
     }
-    const showWikis = () => {
+    const showWiki = () => {
+        result.array.forEach(data => {
+            console.log(result.Data['title']);  
+            wikiList.push(result.Data['title']);
+        });
     }
 
     return(
         <div>
-            
+            { wikiList }
         </div>
     )
 }
