e9a098ab78278a7b36cd55f3a377f8f84e7f57b4
xXAlbob1337Xx <albin.andersson6@elev.ga.ntig.se>
2023-01-11 08:46:40 +0100

    början av delete

---
+++ src/pages/deleteWiki.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { saveLS } from "../components/functions";
+import { loadLS } from "../components/functions";
+import '../App.css';
+import ShowWikis from './ShowWikis'
+
+function deleteWiki(props) {
+    let user = loadLS('user');
+    let token = loadLS('Token');
+    let ID = loadLS('id');
+
+    const API_URL ="https://takeee.ntigskovde.se/Wiki/wiki_index.php?action=deleteWiki&uID="+user+"&token="+token+"&wID="+ID;
+    return(
+        <div>
+            <button onClick={routeChange}>Ta bort wiki</button>
+        </div>
+    )
+    
+}
